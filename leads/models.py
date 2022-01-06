8b2cb2e7cbd4d0408d0aac5bc9e403a9e456735f
jerripat <jerripatterson@gmail.comcore.editor>
2022-01-06 11:59:09 -0600

    updates

--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db.models.fields import CharField
+from django.db.models.fields.files import ImageField
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Lead(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=30)
+    age = models.IntegerField(default=0)
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+
+
+class Agent(models.Model):
+    User = models.OneToOneField(User, on_delete=models.CASCADE)
